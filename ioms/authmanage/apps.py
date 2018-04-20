be899f104677a3d4102f8f0173ee0bd931392f20
tangjianming <93651849@qq.com>
2018-04-20 14:37:33 +0800

    mv ioms-v1 to back

--- ioms/authmanage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthmanageConfig(AppConfig):
-    name = 'authmanage'
--- ioms/usermanage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsermanageConfig(AppConfig):
-    name = 'usermanage'
