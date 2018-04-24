bb0a35b15277acee598a28b94509540350ac4393
tangjianming <93651849@qq.com>
2018-04-24 13:56:43 +0800

    change models

--- ioms/sitecollect/models.py
+++ ioms/sitecollect/models.py
@@ -6,9 +6,9 @@ from django.db import models
 __all__ = ["SiteType", "SiteCollect"]
 
 class SiteType(models.Model):
-	'''
-	site type
-	'''
+    '''
+    site type
+    '''
     typename = models.CharField(max_length=60, null=False, unique=True, verbose_name="网址类型")
 
     def __str__(self):
@@ -16,9 +16,9 @@ class SiteType(models.Model):
 
 
 class SiteCollect(models.Model):
-	'''
-	site collect
-	'''
+    '''
+    site collect
+    '''
     sitename = models.CharField(max_length=60, null=False, unique=True, verbose_name="链接名称")
     siteurl = models.CharField(max_length=60, null=False, unique=True, verbose_name="链接地址")
     typeid = models.ForeignKey(SiteType, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="名称")
