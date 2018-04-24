ae310abce14d72ffbe99fcda0b879fb5e044fa7f
tangjianming <93651549@qq.com>
2018-04-24 17:28:48 +0800

    change url

--- ioms/hostmanage/urls.py
+++ ioms/hostmanage/urls.py
@@ -7,17 +7,38 @@
 # @Mail    : 93651849@qq.com
 
 
-from django.conf.urls import url
+# from django.conf.urls import url
 
-from .views import *
+# from .views import *
 
-app_name = 'hostmanage'
+# app_name = 'hostmanage'
+
+# urlpatterns = [
+#     url(r'^host_index/$', HostIndex.as_view(), name='host_index'),
+#     url(r'^host_detail/(?P<pk>[0-9]+)/$', HostDetailView.as_view(), name='host_detail'),
+#     url(r'^host_detail/project_list/$', ProjectListView.as_view(), name='project_list'),
+#     url(r'^host_detail/cloudplat_list/$', CloudPlatView.as_view(), name='cloudplat_list'),
+#     url(r'^host_detail/role_list/$', RoleView.as_view(), name='role_list'),
+#     #url(r'^host_add', host_add, name='host_add'),
+# ]
+from django.contrib import admin
+from django.urls import path
+
+from .views import SiteIndex, SiteAddView, SiteType, SiteTypeAddView
+
+app_name = 'sitecollect'
 
 urlpatterns = [
-    url(r'^host_index/$', HostIndex.as_view(), name='host_index'),
-    url(r'^host_detail/(?P<pk>[0-9]+)/$', HostDetailView.as_view(), name='host_detail'),
-    url(r'^host_detail/project_list/$', ProjectListView.as_view(), name='project_list'),
-    url(r'^host_detail/cloudplat_list/$', CloudPlatView.as_view(), name='cloudplat_list'),
-    url(r'^host_detail/role_list/$', RoleView.as_view(), name='role_list'),
-    #url(r'^host_add', host_add, name='host_add'),
+    path('site_index/', SiteIndex.as_view(), name='site_index'),
+    path('site_add/', SiteAddView.as_view(), name='site_add'),
+    path('site_type_index/', SiteType.as_view(), name='site_type_index'),
+    path('site_type_add/', SiteTypeAddView.as_view(), name='site_type_add'),
+    # path('admin/', admin.site.urls),
+    # path('login/', Login.as_view(), name='login'), #ok
+    # path('logout/', user_logout, name='logout'),
+    # path('register/', Login.as_view(), name='register'),
+    # # path('user_list/', Login.as_view(), name='user_list'),
+    # path('useradd/', UserAdd.as_view(), name='useradd'),
+    # path('user_list/', UserList.as_view(), name='user_list'),
+    # # path('login/$', )
 ]
