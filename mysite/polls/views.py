8c3e27929dd174f498f018eb806802cc63a0a2d1
tangjianming <93651549@qq.com>
2018-07-06 14:23:44 +0800

    add cbv

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,9 @@ from django.http import Http404, HttpResponseRedirect
 from .models import Question,Choice
 from django.urls import reverse
 
+
+#cbv
+from django.views import generic
 # Create your views here.
 
 from django.http import HttpResponse
@@ -12,6 +15,15 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
 
 
 def detail(request, question_id):
@@ -23,11 +35,22 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
 def results(request, question_id):
     # response = "You're looking at the results of question %s."
     # return HttpResponse(response % question_id)
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 
 def vote(request, question_id):
     # return HttpResponse("You're voting on question %s." % question_id)
