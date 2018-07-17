c4ffe36f911b21b3d7ff83f20b2bb25e46bae942
tangjianming <93651549@qq.com>
2018-07-17 13:23:45 +0800

    update model template

--- ioms/db/models.py
+++ ioms/db/models.py
@@ -8,6 +8,9 @@ class DbType(models.Model):
     type = models.CharField(max_length=48, null=False, verbose_name="type")
     explain = models.CharField(max_length=96, blank=True, verbose_name="说明")
 
+    def __str__(self):
+        return self.type
+
 
 class MasterDb(models.Model):
     id = models.AutoField(primary_key=True)
@@ -17,8 +20,6 @@ class MasterDb(models.Model):
                               verbose_name="host ip's id")
     alias = models.CharField(max_length=48, blank=True, verbose_name="别名")
     db_port = models.CharField(max_length=42, default=3306, verbose_name='database port')
-    # db_slave = models.ForeignKey('self', on_delete=models.SET_NULL, related_name="slave_info", null=True, blank=True,
-    #                              verbose_name="从库")
     status = models.CharField(max_length=42, null=False, verbose_name='database status, online of offline')
     open_time = models.CharField(max_length=42, null=False, blank=True, verbose_name='database open time')
 
--- ioms/db/views.py
+++ ioms/db/views.py
@@ -12,6 +12,17 @@ from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import *
 
+class DbTypeView(LoginRequiredMixin, ListView):
+    template_name = 'db/type_list.html'
+    context_object_name = 'db_type_list'
+    model = DbType
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title_name'] = 'iomp: db config page'
+        return context
+
+
 class DbListView(LoginRequiredMixin, ListView):
     template_name = 'db/db_list.html'
     context_object_name = 'db_list'
